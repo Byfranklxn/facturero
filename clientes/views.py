b9878f50930f77c90d711d0161a03b866bd4bcaf
Byfranklxn <antoniomarriagaf@gmail.com>
2026-07-19 17:44:01 -0500

    Add files via upload

---
+++ clientes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Cliente
+
+def index(request):
+	lista_clientes= Cliente.objects.order_by('nombre')
+	context= {'lista_clientes': lista_clientes}
+	return render(request, 'clientes/tabla.html', context)
